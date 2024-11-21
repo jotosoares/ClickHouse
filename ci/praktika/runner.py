c08e678c4ee1799dd9f7dd07227cfd3229142acd
Max Kainov <max.kainov@clickhouse.com>
2024-11-21 22:10:27 +0100

    CI: Unit tests with praktika

--- ci/praktika/runner.py
+++ ci/praktika/runner.py
@@ -86,6 +86,7 @@ class Runner:
         print("Read GH Environment")
         env = _Environment.from_env()
         env.JOB_NAME = job.name
+        os.environ["JOB_NAME"] = job.name
         env.dump()
         print(env)
 
@@ -200,13 +201,17 @@ class Runner:
                             ResultInfo.TIMEOUT
                         )
                     elif result.is_running():
-                        info = f"ERROR: Job terminated with an error, exit code [{exit_code}]  - set status to [{Result.Status.ERROR}]"
+                        info = f"ERROR: Job killed, exit code [{exit_code}]  - set status to [{Result.Status.ERROR}]"
                         print(info)
                         result.set_status(Result.Status.ERROR).set_info(info)
                     else:
                         info = f"ERROR: Invalid status [{result.status}] for exit code [{exit_code}]  - switch to [{Result.Status.ERROR}]"
                         print(info)
                         result.set_status(Result.Status.ERROR).set_info(info)
+            else:
+                # TODO: add setting with different ways of storing general praktika log: always, on error, never.
+                #   now let's store it on error only
+                result.files.remove(Settings.RUN_LOG)
             result.dump()
 
         return exit_code
@@ -257,10 +262,6 @@ class Runner:
             info = f"ERROR: {ResultInfo.KILLED}"
             print(info)
             result.set_info(info).set_status(Result.Status.ERROR).dump()
-        else:
-            # TODO: add setting with different ways of storing general praktika log: always, on error, never.
-            #   now let's store it on error only
-            result.files = [file for file in result.files if file != Settings.RUN_LOG]
 
         result.update_duration().dump()
 
